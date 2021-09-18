90f8bfd81f673238c646d4b54826a0fecdf79dd6
sagargse <66507809+sagargse@users.noreply.github.com>
2021-09-18 13:41:41 +0530

    initial

--- quiz.js
+++ quiz.js
@@ -1,15 +1,17 @@
-const quizform = document.querySelectorAll(".quiz-form");
+const quizform = document.querySelector("#quiz-form");
 const submitBtn = document.querySelector("#submit");
 const output = document.querySelector("#output");
 
 const correctAnswers = ["180","55"];
 
-function calculateScore() {
-  // console.log("huuiso")
+function calculateScore(e) {
+  // console.log(FormData)
+  e.preventDefault();
   const formResults = new FormData(quizform);
-  console.log(formResults.value)
+
+  console.log(formResults)
   let score = 0;
-      index = 0;
+    let index = 0;
   for (let value of formResults.values()) {
     console.log(value)
     if (value === correctAnswers[index]) {
